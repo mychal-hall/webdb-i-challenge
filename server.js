02a14bf82adbea80b3e62c0190e40fc6afee749d
Mychal Hall <hallmychal@gmail.com>
2019-05-22 21:03:45 -0500

    GET up

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
+const Accounts = require("./data/accounts-model.js");
 // your code here
+
+server.use(express.json());
+
+server.get("/api/accounts", async (req, res) => {
+  try {
+    const accounts = await Accounts.find();
+    res.status(200).json(accounts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error retrieving accounts" });
+  }
+});
+
+
 
 module.exports = server;
