400005d55d77933d987f44071206cdacc7d849e3
Mychal Hall <hallmychal@gmail.com>
2019-05-22 21:42:09 -0500

    Delete UP

--- server.js
+++ server.js
@@ -27,4 +27,34 @@ server.post("/api/accounts", async (req, res) => {
   }
 });
 
+server.get("/api/accounts/:id", async (req, res) => {
+  try {
+    const account = await Accounts.findById(req.params.id);
+    if (account) {
+      res.status(200).json(account);
+    } else {
+      res.status(404).json({ message: "No account with that ID exists" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error retrieving data" });
+  }
+});
+
+server.delete("/api/accounts/:id", async (req, res) => {
+  try {
+    const count = await Accounts.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: "The account has been removed" });
+    } else {
+      res
+        .status(404)
+        .json({ message: "The account with that ID does not exist" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error, could not remove the account" });
+  }
+});
+
 module.exports = server;
